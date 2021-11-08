1ae0dcfccf07360541ea03c28d4f0f95b6163bdb
Hectonite-code <72630507+Hectonite-code@users.noreply.github.com>
2021-11-08 12:13:44 -0500

    Fix small thing in repeat_word.py

--- Challenges/repeat_word.py
+++ Challenges/repeat_word.py
@@ -1,5 +1,5 @@
 def repeated(s):
-    for num in range(2, len(s)):
+    for num in range(1, len(s)):
         parts = [s[i:i+num] for i in range(0, len(s), num)]
         if len(set(parts)) == 1:
             return True
