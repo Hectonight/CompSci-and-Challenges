5630e5edde48c1c6ea933f25429d3b87312ee4dd
Hectonite-code <72630507+Hectonite-code@users.noreply.github.com>
2021-12-23 09:36:55 -0500

    Derivatives :)

---
+++ Challenges/deriv.py
@@ -0,0 +1,24 @@
+equation = input("Equation\n")
+
+
+def derivative(eq):
+    eqO = [oper for oper in str(eq) if oper in ["+", "-"]]
+    eqS = eq.replace("-", "+").split("+")
+    eqD = []
+    eqF = []
+    for sub in eqS:
+        eqD.append(sub.split("x^") if "x^" in sub else sub.split("x"))
+    for sub in eqD:
+        eqF.append(str(int(sub[0]) * int(sub[1])) + "x^" + str(int(sub[1]) - 1))
+    count = 0
+    while len(eqF) > 2:
+        eqF = [eqO[count].join(eqF[0:2])] + eqF[2:]
+        count += 1
+    Final = eqO[-1].join(eqF)
+    Final = Final.replace("x^1+", "x+")
+    Final = Final.replace("x^1-", "x-")
+    Final = Final.replace("x^1-", "x-")
+    return Final
+
+
+print(derivative(equation))
