f79ee739f0d2a940e00ecc3b01896ac1c808c02f
Hectonite-code <72630507+Hectonite-code@users.noreply.github.com>
2021-12-23 11:46:23 -0500

    Derivatives :)

--- Challenges/deriv.py
+++ Challenges/deriv.py
@@ -2,22 +2,41 @@ equation = input("Equation\n")
 
 
 def derivative(eq):
-    eqO = [oper for oper in str(eq) if oper in ["+", "-"]]
-    eqS = eq.replace("-", "+").split("+")
-    eqD = []
+    if "x" not in eq:
+        return "0"
+    eqS = eq.replace("-", "+-")
+    eqS = eqS.replace("^+-", "^-")
+    if eqS.startswith("+"):
+        eqS = eqS[1:]
+
+    eqS = eqS.split("+")
+    for sub in range(len(eqS)):
+        eqS[sub] = eqS[sub].split("x^") if "x^" in eqS[sub] else eqS[sub].split("x")
     eqF = []
     for sub in eqS:
-        eqD.append(sub.split("x^") if "x^" in sub else sub.split("x"))
-    for sub in eqD:
-        eqF.append(str(int(sub[0]) * int(sub[1])) + "x^" + str(int(sub[1]) - 1))
-    count = 0
-    while len(eqF) > 2:
-        eqF = [eqO[count].join(eqF[0:2])] + eqF[2:]
-        count += 1
-    Final = eqO[-1].join(eqF)
-    Final = Final.replace("x^1+", "x+")
-    Final = Final.replace("x^1-", "x-")
+        if len(sub) == 1:
+            continue
+        if sub[1] == "":
+            if sub[0] in ["", "-"]:
+                eqF.append(sub[0]+"1")
+            else:
+                eqF.append(sub[0])
+
+        else:
+            eqF.append(str(int(sub[0]) * int(sub[1])) + "x^" + str(int(sub[1]) - 1))
+
+    print(eqS, eqF)
+    Final = "+".join(eqF)
+    Final.replace("+-", "-")
+    Final = Final.replace("+-", "-")
+    for op in ["+", "-"]:
+        Final = Final.replace(f"x^1{op}", f"x{op}")
+        Final = Final.replace(f"{op}1x", f"{op}x")
+        Final = Final.replace(f"{op}0x", "")
+
     Final = Final.replace("x^0", "")
+    Final = 0 if Final == "" else Final
+
     return Final
 
 
