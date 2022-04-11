793ca4f51b273b6a0c4d7c71d26117118d133c13
Hectonite-code <72630507+Hectonite-code@users.noreply.github.com>
2022-04-11 09:09:46 -0400

    Added a Back Button

--- Challenges/FileExplorer.py
+++ Challenges/FileExplorer.py
@@ -6,7 +6,8 @@ window = Tk()
 window.title("Hectonite Explorer")
 
 
-def setpath():
+def setpath(file):
+    os.chdir(file)
     path.set("Path: " + os.getcwd())
     files.set(os.listdir())
 
@@ -15,25 +16,38 @@ def go():
     index = fileBox.curselection()[0]
     file = files.get()[index]
     try:
-        os.chdir(file)
-        setpath()
+        setpath(file)
     except NotADirectoryError:
         os.system("open " + f'"{file}"' if platform != "win32" else "" + f'"{file}"')
 
 
+def back():
+    newPath = os.getcwd()
+    temp = newPath[:newPath.rfind("/")]
+    if temp != "":
+        newPath = temp
+    elif platform == "win32":
+        return
+    else:
+        newPath = "/"
+    setpath(newPath)
+
+
 path = StringVar()
 files = Variable()
-setpath()
+setpath(os.getcwd())
 Label(window, textvariable=path).grid(row=0, column=0, sticky=W)
+
+Button(window, text="Back", command=back).grid(row=1, column=0, sticky=W)
 
 fileBox = Listbox(window, listvariable=files)
 
 scrollbar = Scrollbar(window)
 fileBox.config(yscrollcommand=scrollbar.set)
-fileBox.grid(row=1, column=0, rowspan=4, sticky=N + E + S + W)
+fileBox.grid(row=2, column=0, rowspan=4, sticky=N + E + S + W)
 fileBox.columnconfigure(0, weight=1)
 scrollbar.config(command=fileBox.yview)
-scrollbar.grid(row=1, column=2, rowspan=4, sticky=N + S)
+scrollbar.grid(row=2, column=2, rowspan=4, sticky=N + S)
 
 fileBox.bind('<Double-1>', lambda event: go())
 
