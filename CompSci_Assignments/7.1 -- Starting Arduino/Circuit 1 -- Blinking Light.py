b4ae1cb1235d89d4fec905c3d87b9a5ea2bfca12
Hectonight <joshuaoring@yahoo.com>
2022-05-26 12:31:43 -0400

    Arduino

---
+++ CompSci_Assignments/7.1 -- Starting Arduino/Circuit 1 -- Blinking Light.py
@@ -0,0 +1,17 @@
+import pyfirmata
+import time
+
+# The right string input here might be different on your computer.
+# Once the Arduino is plugged into your computer, go to
+# the Arduino IDE, then do:
+# Tools > Port and find the port that is similar to the one
+# below (NOT Bluetooth). The numbers on the end might be different.
+# Change the input below so that it matches whatever you see
+# in the Arduino IDE.
+board = pyfirmata.Arduino("/dev/cu.usbmodem14201")
+
+while True:
+    board.digital[13].write(1)
+    time.sleep(1.0)                   # Time that the light is on. You can change this number.
+    board.digital[13].write(0)
+    time.sleep(1.0)                   # Try changing this number too. You can get the blinks to change.
---
+++ CompSci_Assignments/7.1 -- Starting Arduino/Circuit 2 -- Control Button.py
@@ -0,0 +1,21 @@
+import pyfirmata
+import time
+
+# Again, you might need to change the input here.
+# Should be the same as whatever worked in Demo 1.
+board = pyfirmata.Arduino("/dev/cu.usbmodem14201")
+
+# An iterator is a loop that runs "in parallel" with your
+# main code. This loop checks the input from the button.
+iterator = pyfirmata.util.Iterator(board)
+iterator.start()
+
+board.digital[10].mode = pyfirmata.INPUT
+
+while True:
+    button_input = board.digital[10].read()
+    if button_input:
+        board.digital[13].write(1)
+    else:
+        board.digital[13].write(0)
+    time.sleep(0.1)
